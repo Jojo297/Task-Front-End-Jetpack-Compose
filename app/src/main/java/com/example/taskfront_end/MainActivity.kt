bc1e1575a42a53c1832f413bf28aeb1beecee38b
Jojo297 <mafiffudin28@gmail.com>
2024-11-14 13:42:48 +0700

    About page (on progress)

--- app/src/main/java/com/example/taskfront_end/MainActivity.kt
+++ app/src/main/java/com/example/taskfront_end/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.example.taskfront_end.pages.About.AboutPage
 import com.example.taskfront_end.ui.theme.TaskFrontEndTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,3 +26,4 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
--- app/src/main/java/com/example/taskfront_end/pages/About/AboutPage.kt
+++ app/src/main/java/com/example/taskfront_end/pages/About/AboutPage.kt
@@ -2,9 +2,11 @@ package com.example.taskfront_end.pages.About
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,84 +16,167 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.taskfront_end.R
+import com.example.taskfront_end.ui.theme.TaskFrontEndTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AboutPage(modifier: Modifier = Modifier) {
+fun AboutPage(
+    modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5F5F5))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Gambar Profil
+        Image(
+            painter = painterResource(id = R.drawable.user_image), // Ganti dengan gambar profil Anda
+            contentDescription = "Profile Picture",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color(0xFF7650FF), CircleShape)
+                .background(Color.White),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Nama Pengguna
+        Text(
+            text = "Apipu",
+            style = TextStyle(
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF000000)
+            )
+        )
+
+        // Informasi Kontak
+        Text(
+            text = "@mastahh__ | +62 857 6535 1290",
+            style = TextStyle(
+                fontSize = 16.sp,
+                color = Color(0xFF666666)
+            )
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "About Me",
+            style = TextStyle(
+                fontSize = 16.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(600),
+                color = Color(0xFF000000),
+                textAlign = TextAlign.Start,
+                letterSpacing = 0.1.sp,
+            ),
+            modifier = Modifier.align(Alignment.Start)
+        )
 
         Column(
             modifier = Modifier
-                .padding(top = 26.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally, // Mengatur elemen di dalam Column agar berada di tengah
-            verticalArrangement = Arrangement.Center
+                .padding(top = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.Top),
+            horizontalAlignment = Alignment.Start,
         ) {
-            Image(
-                modifier = Modifier
-                    .width(53.dp)
-                    .height(53.dp),
-                painter = painterResource(id = R.drawable.user_image),
-                contentDescription = "Photo Profile",
-                contentScale = ContentScale.FillBounds
-            )
-
-            Text(
-                text = "Itunuoluwa Abidoye",
-                style = TextStyle(
-                    fontSize = 16.sp,
-                    lineHeight = 24.sp,
-                    fontWeight = FontWeight(700),
-                    color = Color(0xFF181D27),
-                    textAlign = TextAlign.Center,
+            // name
+            OutlinedTextField(
+                label = { Text("Name") },
+                value = "Muhammad Afiffudin Al Mahdi",
+                onValueChange = {},
+                enabled = false,
+                readOnly = true,
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    disabledTextColor = Color.Black,
+                    containerColor = Color(0xFFF3F8FF)
                 )
             )
 
-            Text(
-                text = "Itunuoluwa@petra.africa",
-                style = TextStyle(
-                    fontSize = 13.sp,
-                    lineHeight = 19.5.sp,
-                    fontWeight = FontWeight(400),
-                    color = Color(0xFFABABAB),
-                    textAlign = TextAlign.Center,
+            // Email
+            OutlinedTextField(
+                label = { Text("Email") },
+                value = "mafiffudin28@gmail.com",
+                onValueChange = {},
+                enabled = false,
+                readOnly = true,
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    disabledTextColor = Color.Black,
+                    containerColor = Color(0xFFF3F8FF)
                 )
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            // Kampus
+            OutlinedTextField(
+                label = { Text("Perguruan Tinggi") },
+                value = "Politeknik Negeri Batam",
+                onValueChange = {},
+                enabled = false,
+                readOnly = true,
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    disabledTextColor = Color.Black,
+                    containerColor = Color(0xFFF3F8FF)
+                )
+            )
 
-            Column {
-                Text(
-                    text = "Name"
+            // Jurusan
+            OutlinedTextField(
+                label = { Text("Jurusan") },
+                value = "Teknik Informatika",
+                onValueChange = {},
+                enabled = false,
+                readOnly = true,
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    disabledTextColor = Color.Black,
+                    containerColor = Color(0xFFF3F8FF)
                 )
-                Text(
-                    text = "Email"
-                )
-                Text(
-                    text = "Asal Perguruan Tinggi"
-                )
-                Text(
-                    text = "Jurusan"
-                )
-            }
+            )
         }
-
     }
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    TaskFrontEndTheme {
+        AboutPage()
+    }
+}
